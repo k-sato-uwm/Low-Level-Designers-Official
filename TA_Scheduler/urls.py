701fa015d1745ea59a90d688554ddb92680e8466
Keizan <keizan.sato@gmail.com>
2024-12-02 01:32:25 -0600

    Implemented views for dashboard

--- TA_Scheduler/urls.py
+++ TA_Scheduler/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from scheduler.views import Dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', Dashboard.as_view()),
 ]
