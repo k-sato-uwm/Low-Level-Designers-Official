492170bc3f2212dc8793d6b562a5f675aaf213e5
MRSX <alvkondr@gmail.com>
2024-12-02 14:28:44 -0600

    bruh

--- scheduler/views.py
+++ scheduler/views.py
@@ -7,12 +7,19 @@ from scheduler.models import Course
 class CourseManagement(View):
     template_name = 'CourseManagement.html'
 
-    def get(self, request):
 
+
+
+    def get(self, request):
         courses = Course.objects.all()
         print(courses)
         context = {'courses': courses}
-        return render(request,'CourseManagement.html',context)
+        return render(request,self.template_name,context)
 
     def post(self, request):
-        return render(request,'CourseManagement.html')
+        print(request.POST)
+
+
+
+
+        return self.get(request)
