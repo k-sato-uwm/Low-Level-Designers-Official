1916d1b174c4413aedc503113b3eef60ab500e4b
Keizan <keizan.sato@gmail.com>
2024-12-04 01:53:38 -0600

    Wrote acceptance tests for dashboard

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -3,6 +3,48 @@ from django.urls import reverse
 from scheduler.models import Course, User
 from django.contrib.messages import get_messages
 
+class DashboardTests(TestCase):
+    def setup(self):
+        self.client = Client()
+
+        ta = User(username='TestTA', password='pass123', role='Teaching Assistant')
+        ta.save()
+        inst = User(username='TestInst', password='pass123', role='Instructor')
+        inst.save()
+        admin = User(username='TestAdmin', password='pass123', role='Administrator')
+        admin.save()
+
+    def test_signedinAdmin(self): # If the user signs in as an Admin
+        res = self.client.post('/', {'username': 'TestAdmin', 'password': 'pass123'}, follow=True)
+        self.assertEqual(self.client.session['username'], 'TestAdmin')
+        self.assertEqual(self.client.session['role'], 'Administrator')
+        self.assertRedirects(res, '/dashboard/', 302)
+        self.assertTemplateUsed(res, template_name='SupervisorDash.html')
+        self.assertEqual(res.context['name'], 'TestAdmin')
+
+    def test_signedinInstructor(self): # If the user signs in as an Instructor
+        res = self.client.post('/', {'username': 'TestInst', 'password': 'pass123'}, follow=True)
+        self.assertEqual(self.client.session['username'], 'TestInst')
+        self.assertEqual(self.client.session['role'], 'Instructor')
+        self.assertRedirects(res, '/dashboard/', 302)
+        self.assertTemplateUsed(res, template_name='InstDash.html')
+        self.assertEqual(res.context['name'], 'TestInst')
+
+    def test_signedinTA(self): # If the user signs in as a TA
+        res = self.client.post('/', {'username': 'TestTA', 'password': 'pass123'}, follow=True)
+        self.assertEqual(self.client.session['username'], 'TestTA')
+        self.assertEqual(self.client.session['role'], 'Teaching Assistant')
+        self.assertRedirects(res, '/dashboard/', 302)
+        self.assertTemplateUsed(res, template_name='TADash.html')
+        self.assertEqual(res.context['name'], 'TestTA')
+
+    def test_notsignedin(self): # If the user is not signed in
+        res = self.client.get('/dashboard/', follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertRedirects(res, '/', 302)
+
+
+
 class CourseManagementTests(TestCase):
     def setUp(self):
         self.client = Client()
--- scheduler/views.py
+++ scheduler/views.py
@@ -119,26 +119,28 @@ class LoginView(View):
 
 class Dashboard(View):
     def get(self, request):
-        userrole = request.session['user']['role']
+        userrole = request.session['role']
+        username
 
         match userrole:
             case 'Supervisor':
-                return render(request, "SupervisorDash.html", {"name": request.session['user']['username']})
+                return render(request, "SupervisorDash.html", {"name": request.session['username']})
             case 'Instructor':
-                return render(request, "InstDash.html", {"name": request.session['user']['username']})
+                return render(request, "InstDash.html", {"name": request.session['username']})
             case 'Teaching Assistant':
-                return render(request, "TADash.html", {"name": request.session['user']['username']})
+                return render(request, "TADash.html", {"name": request.session['username']})
 
     def post(self, request): #Should not receive POST requests on dashboard page
-        userrole = request.session['user']['role']
-
-        match userrole:
-            case 'Supervisor':
-                return render(request, "SupervisorDash.html", {"name": request.session['user']['username']})
-            case 'Instructor':
-                return render(request, "InstDash.html", {"name": request.session['user']['username']})
-            case 'Teaching Assistant':
-                return render(request, "TADash.html", {"name": request.session['user']['username']})
+        pass
+        # userrole = request.session['role']
+        #
+        # match userrole:
+        #     case 'Supervisor':
+        #         return render(request, "SupervisorDash.html", {"name": request.session['username']})
+        #     case 'Instructor':
+        #         return render(request, "InstDash.html", {"name": request.session['username']})
+        #     case 'Teaching Assistant':
+        #         return render(request, "TADash.html", {"name": request.session['username']})
 
 class UserManagementView(View):
     # creates an instance of userManagement and assigns it to user_manager
