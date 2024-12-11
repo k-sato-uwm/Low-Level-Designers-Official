27f13f028da06d103e4342eafff8118fde84377c
Keizan <keizan.sato@gmail.com>
2024-12-10 19:53:40 -0600

    Created an assignment table for many-to-many relationships

--- scheduler/models.py
+++ scheduler/models.py
@@ -25,19 +25,26 @@ class User(models.Model):
 # Lab Model
 class Lab(models.Model):
     lab_id = models.AutoField(primary_key=True) #Unigue id, for now just pk
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='labs')
+    # course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='labs')
     section_number = models.CharField(max_length=25) #EX 808,801
-    ta = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='labs_as_ta')
+    # ta = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='labs_as_ta')
 
     def __str__(self):
-        return f'Lab {self.section_number} for Course {self.course.course_name}'
+        return f'Lab {self.section_number}'
 
 # Course Model
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True) #Unigue id, for now just pk
     course_code = models.CharField(max_length=20, unique=True) #Ex CS101
     course_name = models.CharField(max_length=50) #Ex. Intro to SE
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_as_instructor')
+    # instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_as_instructor')
 
     def __str__(self):
         return self.course_name
+
+# Assignment Table
+class Assignments(models.Model):
+    assignment_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assignments') # User and course are required
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='users')
+    lab_id = models.ForeignKey(Lab, on_delete=models.CASCADE, related_name='users', null=True, default = '') # Lab can be null (eg user is assigned to course and not a lab)
